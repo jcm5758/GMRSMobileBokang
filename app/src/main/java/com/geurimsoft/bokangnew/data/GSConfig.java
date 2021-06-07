a0e5a79b4079b66db5fb7b4d235a0f3e1001f2f7
Kijung Lee <jcm5758@gmail.com>
2021-06-07 15:43:11 +0900

    202106071543

--- app/src/main/java/com/geurimsoft/bokangnew/data/GSConfig.java
+++ app/src/main/java/com/geurimsoft/bokangnew/data/GSConfig.java
@@ -34,7 +34,7 @@ public class GSConfig
     public static UserInfo CURRENT_USER = null;
 
     // Acitivity 리스트 : 지점 수정할 때 수정해야 함
-    public static Class[] Activity_LIST = new Class[]{ GwangjuTabActivity.class, com.geurimsoft.bokangnew.view.joomyung.ActivityMain.class};
+    public static Class[] Activity_LIST = new Class[]{ com.geurimsoft.bokangnew.view.kwangju.ActivityMain.class, com.geurimsoft.bokangnew.view.joomyung.ActivityMain.class};
 
     public static String LOG_MSG(String className, String funcName)
     {
