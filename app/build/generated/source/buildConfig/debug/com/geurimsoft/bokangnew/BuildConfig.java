4c1f4ff14a783b8b1d4e338a30c6674bac4401a2
Kijung Lee <jcm5758@gmail.com>
2021-06-09 17:08:48 +0900

    202106091708

--- app/build/generated/source/buildConfig/debug/com/geurimsoft/bokangnew/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/geurimsoft/bokangnew/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.geurimsoft.bokangnew";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 104;
-  public static final String VERSION_NAME = "1.0.4";
+  public static final int VERSION_CODE = 105;
+  public static final String VERSION_NAME = "1.0.5";
 }
--- app/src/main/java/com/geurimsoft/bokangnew/view/util/DayDatePickerDialog.java
+++ app/src/main/java/com/geurimsoft/bokangnew/view/util/DayDatePickerDialog.java
@@ -4,7 +4,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
