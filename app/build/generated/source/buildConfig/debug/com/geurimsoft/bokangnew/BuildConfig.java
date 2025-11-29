42c5c485208c03f77129fcb2c2f14838f8390382
Kijung Lee <jcm5758@gmail.com>
2025-11-29 12:35:18 +0900

    202511291234

--- app/build/generated/source/buildConfig/debug/com/geurimsoft/bokangnew/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/geurimsoft/bokangnew/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.geurimsoft.bokangnew";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 112;
-  public static final String VERSION_NAME = "1.1.2";
+  public static final int VERSION_CODE = 120;
+  public static final String VERSION_NAME = "1.2.0";
 }
